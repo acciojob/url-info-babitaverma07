59a3bcfa803c4d784b48788fdf2bf0803d1fa955
Babita Verma <147910203+babitaverma07@users.noreply.github.com>
2024-08-31 22:53:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+console.log(isDate(new Date())); // Output: true
+console.log(isDate("2023-08-11")); // Output: true
+console.log(isDate("not a date")); // Output: false
